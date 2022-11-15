34f79b81fc23b6303c3a3e652a1dba157986bc49
Michael Chan <michaelcychan@gmail.com>
2022-11-15 14:31:13 +0000

    refacotr board test, cell test and integration test, add populate method to board class, cell have make_alive and kill method

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,24 @@
 class Board
-  def initialize()
-    @board = Array.new(8).map{ |x| Array.new(8, '💀')}
+  def initialize(cell_class)
+    @board = Array.new(8).map{ |x| Array.new(8).map{|y| cell_class.new()}}
+  end
+
+  def get_board()
+    @board
   end
 
   def show_board()
-    return @board
+    return @board.map do | row|
+      row.map do | cell |
+        cell.get_status
+      end
+    end
   end
 
   def populate(cells_pos)
     cells_pos.each do |cell_pos|
-      @board[cell_pos[0]][cell_pos[1]] = '🐣'
+      @board[cell_pos[0]][cell_pos[1]].make_alive()
     end
   end
 
-  
 end
