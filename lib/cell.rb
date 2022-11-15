d817ca027aa43304c9f3fe5d485c48752c6d4cfc
Michael Chan <michaelcychan@gmail.com>
2022-11-15 12:28:35 +0000

    add position to cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,10 @@
 class Cell
+  def initialize(x_pos, y_pos)
+    @pos = [x_pos, y_pos]
+  end
 
+  def get_pos()
+    return @pos
+  end
+  
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,8 +3,9 @@ require 'cell'
 RSpec.describe 'Cell' do
   describe 'successfully initiate a Cell class instance' do
     it 'creates a cell object' do
-      cell = Cell.new
+      cell = Cell.new(1, 2)
       expect(cell).to be_a Cell
+      expect(cell.get_pos()).to eq([1,2])
     end
   end
 end
