728ca9124e71a3536fcc49f326f434aac210df71
Michael Chan <michaelcychan@gmail.com>
2022-11-15 13:12:28 +0000

    change @board initialize method, can populate board with cells

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+  def initialize()
+    @board = Array.new(8).map{ |x| Array.new(8, '💀')}
+  end
+
+  def show_board()
+    return @board
+  end
+
+  def populate(cells_pos)
+    cells_pos.each do |cell_pos|
+      @board[cell_pos[0]][cell_pos[1]] = '🐣'
+    end
+  end
+end
