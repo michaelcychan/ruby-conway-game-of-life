4ffca53ad1766d9c77f2bcf2fe74e34febfe17ac
Michael Chan <michaelcychan@gmail.com>
2022-11-17 14:38:06 +0000

    print individual cell instead of inside an array

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,31 @@
 require_relative 'board'
 require_relative 'cell'
 
-board = Board.new(Cell)
+puts "Welcome to Conway's Game of Life"
+user_input = " "
+while !(user_input.match(/\d{1,2}/) && user_input.to_i >=6 && user_input.to_i <=20) do
+  puts "Please enter a square board size (6 - 20): "
+  user_input = gets.chomp
+end 
+
+
+board = Board.new(Cell, user_input.to_i)
 board.populate([[1,0], [1,1], [1,2], [3, 5], [4, 3], [4, 4], [4, 5], [5, 5]])
 
 while true do
   system('clear')
   board.show_board.each do |row|
-    print row
+    for i in 0..(row.length-1) do
+      print row[i]
+      print " "
+    end
     print "\n"
   end
   
   print "\n\n\n"
   print "Press Ctrl + C to exit"
 
-  sleep(0.5)
+  sleep(1)
   
   board.next_round
 end
